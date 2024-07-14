9c88a9e5cd90f82e4e46ccbd0a345fc4bd821473
enix403 <fog.code000@gmail.com>
2024-07-15 00:53:17 +0500

    Set template

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,svx,md,ts}'],
 
   theme: {
     extend: {}
   },
 
-  plugins: []
+  plugins: [require("rippleui")]
 } satisfies Config;
