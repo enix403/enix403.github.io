8a211e2c86eb6176b96d72ebfd88ea806c5875b5
enix403 <fog.code000@gmail.com>
2024-07-19 01:56:18 +0500

    responsive

--- src/lib/components/PostLink.svelte
+++ src/lib/components/PostLink.svelte
@@ -1,4 +1,4 @@
-<div class="flex items-start gap-x-4 border-b border-app-line py-4 first:pt-0">
+<div class="flex items-start gap-x-4 gap-y-2 border-b border-app-line py-4 first:pt-0 max-md:flex-col-reverse">
   <p class="text-app-muted mt-2 shrink-0 text-nowrap font-mono text-[0.7rem] uppercase">Aug 03</p>
   <h2 class="max-w-2xl md:text-lg lg:text-xl">
     Implementing And Understanding NeRF with too many loops in python
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <h1 class="mb-7 border-b border-app-line pb-3 text-4xl font-semibold">Blogs</h1>
 
 <div class="flex items-start gap-x-5">
-  <div class="text-app-muted text-xl tracking-wider">2022</div>
+  <div class="text-app-muted text-xl tracking-wider max-sm:hidden">2022</div>
   <div class="flex-1">
     <PostLink />
     <PostLink />
