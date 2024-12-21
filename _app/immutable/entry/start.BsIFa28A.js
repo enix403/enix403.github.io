beccbfc448b5500ae399120b74405457f7322477
enix403 <fog.code000@gmail.com>
2024-12-21 23:03:22 +0500

    Updates

---
+++ _app/immutable/entry/start.BsIFa28A.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DX21vuvC.js";export{t as start};
---
+++ _app/immutable/nodes/2.DBzN2KjQ.js
@@ -0,0 +1,1 @@
+import{a as t,t as e}from"../chunks/disclose-version.DagJWH28.js";import"../chunks/legacy.PCdeuNic.js";var p=e("<h1>This is the home page</h1>");function m(a){var o=p();t(a,o)}export{m as component};
