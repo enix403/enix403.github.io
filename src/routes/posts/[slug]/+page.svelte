6446df412553998911a4af7f9659f2df800b6880
enix403 <fog.code000@gmail.com>
2024-12-21 21:23:44 +0500

    Restructure

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
   import type { PageData } from './$types';
-  import Post from '$lib/components/Post.svelte';
-
   const { data }: { data: PageData } = $props();
   const { frontmatter, ContentBody } = data;
 </script>
 
-<Post {frontmatter} {ContentBody} />
+<h1>
+  <strong>
+    {frontmatter.title}
+  </strong>
+</h1>
+
+<hr />
+
+<main class="flex max-w-full justify-center px-4 lg:px-24 py-4">
+  <div class="w-full max-w-[800px] pb-72 post-content">
+    <ContentBody />
+  </div>
+</main>
+
+<style>
+  .post-content :global {
+    pre {
+      @apply overflow-x-auto;
+      @apply px-2.5 py-2;
+    }
+
+    .math-display {
+      @apply overflow-x-auto;
+      @apply px-4;
+    }
+  }
+</style>
