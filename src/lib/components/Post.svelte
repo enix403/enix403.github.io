7af72e5dba4dd68c45035d811aa48d67b9f6b764
enix403 <fog.code000@gmail.com>
2024-12-21 21:07:53 +0500

    Post page

---
+++ src/lib/components/Post.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" module>
+  export type Frontmatter = {
+    title: string;
+    slug: string;
+    date: string;
+  };
+</script>
+
+<script lang="ts">
+  import type { Component } from 'svelte';
+  let {
+    frontmatter,
+    ContentBody
+  }: {
+    frontmatter: Frontmatter;
+    ContentBody: Component;
+  } = $props();
+</script>
+
+<h1>
+  <strong>
+    {frontmatter.title}
+  </strong>
+</h1>
+
+<hr />
+
+<ContentBody />
--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
+  import type { PageData } from './$types';
+  import Post from '$lib/components/Post.svelte';
 
   const { data }: { data: PageData } = $props();
   const { frontmatter, ContentBody } = data;
 </script>
 
-<h1>
-  This is some post
-</h1>
-
-<p>
-  {JSON.stringify(frontmatter)}
-</p>
-
-<ContentBody />
+<Post {frontmatter} {ContentBody} />
